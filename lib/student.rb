38bd25c9060103c78f53f122a8ed88013fb0328d
Toni-Ty <antonique2479@yahoo.com>
2019-08-21 04:43:47 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,6 +28,13 @@ class Student
     DB[:conn].execute(sql)
   end
 
-
+  def save 
+    if self.id 
+      self.update
+    else 
+      sql = <<-SQL 
+      INSERT INTO students (name, grade)
+      VALUES (?,?)
+      SQL
 
 end
